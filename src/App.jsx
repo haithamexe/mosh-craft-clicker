9b758402fcab734024b3ff91d8b6abdbbf8a0244
haithamexe <haitham9559@gmail.com>
2025-09-08 01:39:07 +0300

    fixed cat counter and added socials

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import Card from "./components/Card";
 import MovingCard from "./components/MovingCard";
+import { FaTwitch } from "react-icons/fa";
+import { FaYoutube } from "react-icons/fa";
+import { FaDiscord } from "react-icons/fa";
 
 function App() {
   const [sirPower, setSirPower] = useState(4);
@@ -39,7 +42,7 @@ function App() {
 
   useEffect(() => {
     calculateTotalPower();
-  }, [sirPower]);
+  }, [sirPower, koshPower, endregas]);
 
   const calculateTotalPower = () => {
     let endregasPower = 0;
@@ -60,16 +63,28 @@ function App() {
   }, [koshPower]);
 
   return (
-    <div className="bg-black min-h-screen font-gwent ">
-      <section className="bg-red-900 rounded-b-3xl mb-4 flex items-center justify-between px-12">
-        <h1 className="text-4xl text-center font-bold text-white p-4">
-          Gwent: Moshcraft Experience.exe
-        </h1>
+    <div className="font-gwent ">
+      <section className="bg-body rounded-b-4xl mb-4 flex items-center justify-between px-12 fixed top-0 left-0 w-full z-50 border-b-4 border-red-900">
+        <div className="flex items-center gap-4 py-4 cursor-pointer">
+          <img
+            src="./images/mosh.png"
+            alt="mosh logo"
+            className="w-15 h-auto object-contain "
+          />
+          {/* <h1 className="text-4xl text-center font-bold text-white p-4">
+            Gwent: Moshcraft Experience.exe
+          </h1> */}
+          <img
+            src="./images/gwetext.png"
+            alt="mosh logo"
+            className="w-150 h-auto object-contain"
+          />
+        </div>
         <h2 className="text-2xl text-center font-semibold text-white p-4">
           Moshed Points: {totalPower}
         </h2>
       </section>
-      <section className="flex gap-[0.2rem] p-14 flex-wrap justify-left relative">
+      <section className="flex gap-[0.2rem] p-14 flex-wrap justify-left relative mt-20">
         <MovingCard
           power={sirPower}
           img={sirImg}
@@ -81,6 +96,34 @@ function App() {
         {endregas.map((endrega) => (
           <Card key={endrega.id} power={endrega.power} img={endrega.img} />
         ))}
+      </section>
+      <img
+        src="./images/back.jpg"
+        alt="mosh logo"
+        className="w-full h-full object-cover fixed top-2 left-0 -z-10 "
+      />
+      <section className="fixed bottom-4 right-4 flex gap-5 text-4xl bg-black px-4 py-2 rounded-full ">
+        <a
+          href="https://discord.com/invite/5r4W94HKWT"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FaDiscord className="text-purple-500 hover:text-purple-400 cursor-pointer transition-all" />
+        </a>
+        <a
+          href="https://www.twitch.tv/moshcraft"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FaTwitch className="text-purple-500 hover:text-purple-400 cursor-pointer transition-all" />
+        </a>
+        <a
+          href="https://www.youtube.com/channel/UC63UktC9K6ACRv5z1fPcSmg"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FaYoutube className="text-red-700 hover:text-red-400 cursor-pointer transition-all" />
+        </a>
       </section>
     </div>
   );
